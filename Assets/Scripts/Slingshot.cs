c206f05a649c47cbe99372db6c3937352d5c4642
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2018-09-22 11:11:28 -0400

    b e a utiful castles

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -104,6 +104,8 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotsFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 }
